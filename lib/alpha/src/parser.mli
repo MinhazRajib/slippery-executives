c57199f45cac1413d87f03a7efb0af9916878a39
Minhaz Rajib <rajib.minhaz@yahoo.com>
2026-07-23 20:23:20 +0000

    parser complete

--- lib/alpha/src/parser.mli
+++ lib/alpha/src/parser.mli
@@ -1,11 +1,13 @@
 open! Core
 open! Execlab_types
 
-type t = { instructions : list Alpha_instruction.t }
+type t = { instructions :  Alpha_instruction.t list}
 [@@deriving sexp, bin_io, compare, equal]
 
 
 (*expected format:
-
+    arrival_time,symbol,side,quantity,deadline
+    09:30:00,AAPL,BUY,100,10:15:00]
 *)
-val parse : string -> (Alpha_instruction list, Error.t list) result
+
+val parse : string -> t Or_error.t
